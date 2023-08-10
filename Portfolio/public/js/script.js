2371c2c8897b353148d8fc827256670608ee22dc
cheikhdiopfaty <124917802+cheikhdiopfaty@users.noreply.github.com>
2023-08-10 16:55:46 +0000

    Add files via upload

---
+++ Portfolio/public/js/script.js
@@ -0,0 +1,20 @@
+
+       
+
+const menuicon = document.querySelector("#menuIcon");
+
+ function handleMenu(){
+     
+     const menu = document.querySelector(".menu");
+ menu.classList.toggle('hide')
+}
+
+
+
+// const menuicon = document.querySelector("#menuIcon");
+// const menu = document.querySelector(".menu");
+
+// menuicon.addEventListener("click", () =>{
+//     menuicon.classList.toggle("hide");
+//     menu.classList.toggle("hide");
+// })
